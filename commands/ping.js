f2d9e8e729d76854b2a0ae0253a1fcefd259d1a4
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2022-01-07 01:04:44 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,13 @@
 const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  return message.channel.send("Pong!");
+  return message.channel.send(
+    "**SMD** Officiele Discord Bot \n play.smdserver.nl -> 1.18"
+  );
 };
 
 module.exports.help = {
-  name: "ping",
+  name: "ip",
   category: "general",
-  description: "Pong.",
+  description: "Server IP.",
 };
